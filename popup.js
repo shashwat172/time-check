6974f059a15fc6938ca51dad2318dd0201d09d61
Shashwat Mishra <58601580+shashwat172@users.noreply.github.com>
2020-12-26 20:28:46 +0530

    kucch to kiya

---
+++ popup.js
@@ -0,0 +1,9 @@
+var node = document.createElement('li');
+node.appendChild(document.createTextNode('Scooter'));
+ 
+document.querySelector('#urlwt').appendChild(node);
+
+$(function(){
+    
+    $("#urlwt").createElement('li').appendChild(document.createTextNode('url'));
+});
